0ffebc12a88e326517c2acf7c447fe9e4a3bc01e
Waveform Labs <hello.waveformlabs@gmail.com>
2026-05-03 10:22:31 +1200

    fix: bump db version to 4 to reseed image names

--- app/src/main/java/nz/keeleysgreenhouse/app/data/AppDatabase.kt
+++ app/src/main/java/nz/keeleysgreenhouse/app/data/AppDatabase.kt
@@ -34,7 +34,7 @@ import nz.keeleysgreenhouse.app.data.entity.UserPlanting
         Favourite::class,
         SearchHistory::class
     ],
-    version = 3,
+    version = 4,
     exportSchema = false
 )
 @TypeConverters(Converters::class)
